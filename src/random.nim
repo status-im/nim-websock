76a16763e4d7d06bd86eee3d381a77899bcabb65
Dmitriy Ryajov <dryajov@gmail.com>
2021-03-10 16:57:46 -0600

    use byte for more comfort

--- src/random.nim
+++ src/random.nim
@@ -3,6 +3,7 @@ import bearssl
 ## Random helpers: similar as in stdlib, but with BrHmacDrbgContext rng
 const randMax = 18_446_744_073_709_551_615'u64
 
+
 proc rand*(rng: var BrHmacDrbgContext, max: Natural): int =
   if max == 0: return 0
   var x: uint64
@@ -16,9 +17,9 @@ proc genMaskKey*(rng: ref BrHmacDrbgContext): array[4, char] =
   proc r(): char = char(rand(rng[], 255))
   return [r(), r(), r(), r()]
 
-proc genWebSecKey*(rng: ref BrHmacDrbgContext): seq[char] =
-  var key = newSeq[char](16)
-  proc r(): char = char(rand(rng[], 255))
+proc genWebSecKey*(rng: ref BrHmacDrbgContext): seq[byte] =
+  var key = newSeq[byte](16)
+  proc r(): byte = byte(rand(rng[], 255))
   ## Generates a random key of 16 random chars.
   for i in 0..15:
     key.add(r())
