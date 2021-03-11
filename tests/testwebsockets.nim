5173555f709677f83373f08166b4921c1b363d83
Dmitriy Ryajov <dryajov@gmail.com>
2021-03-10 23:41:15 -0600

    more tests

--- tests/testwebsockets.nim
+++ tests/testwebsockets.nim
@@ -132,3 +132,65 @@ suite "Test transmission":
 
     let res = await ws.recv()
     check string.fromBytes(res) == testString
+
+suite "Test framing":
+  teardown:
+    httpServer.stop()
+    await httpServer.closeWait()
+
+  test "should split message into frames":
+    let testString = "1234567890"
+    var done = newFuture[void]()
+    proc cb(transp: StreamTransport, header: HttpRequestHeader) {.async.} =
+      check header.uri() == "/ws"
+
+      let ws = await createServer(header, transp, "proto")
+
+      let frame1 = await ws.readFrame()
+      check not isNil(frame1)
+      var data1 = newSeq[byte](frame1.remainder().int)
+      let read1 = await ws.tcpSocket.readOnce(addr data1[0], data1.len)
+      check read1 == 5
+
+      let frame2 = await ws.readFrame()
+      check not isNil(frame2)
+      var data2 = newSeq[byte](frame2.remainder().int)
+      let read2 = await ws.tcpSocket.readOnce(addr data2[0], data2.len)
+      check read2 == 5
+
+      await transp.closeWait()
+      done.complete()
+
+    httpServer = newHttpServer("127.0.0.1:8888", cb)
+    httpServer.start()
+
+    let ws = await connect(
+      "127.0.0.1",
+      Port(8888),
+      path = "/ws",
+      protocols = @["proto"],
+      frameSize = 5)
+
+    await ws.send(testString)
+    await done
+
+  test "should fail to read past max message size":
+    let testString = "1234567890"
+    proc cb(transp: StreamTransport, header: HttpRequestHeader) {.async.} =
+      check header.uri() == "/ws"
+
+      let ws = await createServer(header, transp, "proto")
+      await ws.send(testString)
+      await transp.closeWait()
+
+    httpServer = newHttpServer("127.0.0.1:8888", cb)
+    httpServer.start()
+
+    let ws = await connect(
+      "127.0.0.1",
+      Port(8888),
+      path = "/ws",
+      protocols = @["proto"])
+
+    expect WSMaxMessageSizeError:
+      discard await ws.recv(5)
