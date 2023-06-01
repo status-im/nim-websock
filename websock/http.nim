7a46d58a714e23bc6faa4b60176c162a7572b5b5
tersec <tersec@users.noreply.github.com>
2023-06-01 13:44:07 +0000

    modify more, maybe it won't skip autobahn test suite

--- websock/http.nim
+++ websock/http.nim
@@ -8,11 +8,12 @@
 ## those terms.
 
 import std/uri
+
 import pkg/[
   chronos/apps/http/httptable,
   chronos/streams/tlsstream,
   httputils]
 
-import ./http/client, ./http/server, ./http/common
+import  ./http/client, ./http/server, ./http/common
 
 export uri, httputils, client, server, httptable, tlsstream, common
